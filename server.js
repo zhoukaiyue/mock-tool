26697c61327743590f58672bb4e29f98f774fa93
zhoukaiyue <1301524439@qq.com>
2024-02-07 10:00:58 +0800

    test:111

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: zhoukai
+ * @Date: 2024-02-01 17:05:49
+ * @LastEditors: zhoukai
+ * @LastEditTime: 2024-02-07 09:50:07
+ */
 require('./src/main') //默认
 
 // require('./src/main.cors') //使用cors中间件解决跨域问题
--- src/main.proxy.js
+++ src/main.proxy.js
@@ -1,12 +1,27 @@
-//参考：https://www.runoob.com/nodejs/nodejs-express-framework.html
-//express_demo.js 文件
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: zhoukai
+ * @Date: 2024-01-26 16:50:59
+ * @LastEditors: zhoukai
+ * @LastEditTime: 2024-02-01 17:51:10
+ */
 var express = require('express')
 var app = express()
 var path = require('path')
 
-//导入工具函数：读取指定目录下所有文件
+// var cors = require('cors') // 引入 cors 模块
+// // 使用 cors 中间件，并配置允许的域名
+// app.use(
+//     cors({
+//         // origin: 设置多个 允许跨域的域名
+//         origin: 'http://demo.127516.com,http://demo.127516.com', // 只允许 http://localhost:23001 这个域名访问
+//     })
+// )
+
+// 导入工具函数：读取指定目录下所有文件
 const $fs = require('./util/fs')
-//导出所有项目的功能接口模块
+// 导出所有项目的功能接口模块
 var filesList = []
 $fs.readFileList(path.join(__dirname, '..', 'src/project'), filesList)
 
